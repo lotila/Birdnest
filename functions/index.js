4866a54b726f39946885762695e892411bb096eb
tomi lotila <lotilatomi@gmail.com>
2022-12-19 17:23:47 +0200

    local host is working

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,3 @@
-const functions = require("firebase-functions");
 
 // // Create and deploy your first functions
 // // https://firebase.google.com/docs/functions/get-started
@@ -7,3 +6,36 @@ const functions = require("firebase-functions");
 //   functions.logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
+
+const functions = require('firebase-functions');
+const express = require('express');
+const engines = require('consolidate');
+var hbs = require('handlebars');
+const admin = require('firebase-admin');
+
+const app = express();
+app.engine('hbs',engines.handlebars);
+app.set('views','./views');
+app.set('view engine','hbs');
+
+
+var serviceAccount = require("../../dronetracking.json");
+admin.initializeApp({
+credential: admin.credential.cert(serviceAccount),
+});
+
+async function getFirestore(){
+    const firestore_con  = await admin.firestore();
+    const writeResult = firestore_con.collection('sample').doc('sample_doc').get().then(doc => {
+    if (!doc.exists) { console.log('No such document!'); }
+    else {return doc.data();}})
+    .catch(err => { console.log('Error getting document', err);});
+    return writeResult
+    }
+
+
+app.get('/',async (request,response) =>{
+var db_result = await getFirestore();
+response.render('index',{db_result});
+});
+exports.app = functions.https.onRequest(app);
